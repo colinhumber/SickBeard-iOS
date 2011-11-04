ed55e4989af9995008c576ad22f784bab2d7a174
Colin Humber <colinhumber@gmail.com>
2011-11-03 22:47:43 -0600

    Test

--- SickBeard/Classes/SBEpisodesViewController.m
+++ SickBeard/Classes/SBEpisodesViewController.m
@@ -26,7 +26,7 @@
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
     
-    // Release any cached data, images, etc that aren't in use.
+    // Release any cached data, images, etc that aren't in use. 
 }
 
 
