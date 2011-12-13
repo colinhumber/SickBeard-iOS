b54925be5984757242b9b3a7a91fd6eb1384e986
Colin Humber <colinhumber@gmail.com>
2011-12-13 15:10:12 -0700

    Removed username/password from server and validations; added swiping to episode details

---
+++ SickBeard/Classes/SBEpisodeDetailsHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  SBEpisodeDetailsHeaderView.h
+//  SickBeard
+//
+//  Created by Colin Humber on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBEpisodeDetailsHeaderView : UIView
+
+@end
---
+++ SickBeard/Classes/SBEpisodeDetailsHeaderView.m
@@ -0,0 +1,31 @@
+//
+//  SBEpisodeDetailsHeaderView.m
+//  SickBeard
+//
+//  Created by Colin Humber on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SBEpisodeDetailsHeaderView.h"
+
+@implementation SBEpisodeDetailsHeaderView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
