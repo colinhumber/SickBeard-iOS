2ffe9246961868ca7b6877f8878617f00f9a47c9
Colin Humber <colinhumber@gmail.com>
2013-02-22 15:33:28 -0700

    moved submodule "SickBeard/JSONKit" to "Vendor/JSONKit" (url: https://github.com/johnezang/JSONKit.git)

---
+++ SickBeard/SBNotificationManager.h
@@ -0,0 +1,13 @@
+//
+//  SBNotificationManager.h
+//  SickBeard
+//
+//  Created by Colin Humber on 2/22/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBNotificationManager : NSObject
+
+@end
---
+++ SickBeard/SBNotificationManager.m
@@ -0,0 +1,13 @@
+//
+//  SBNotificationManager.m
+//  SickBeard
+//
+//  Created by Colin Humber on 2/22/13.
+//
+//
+
+#import "SBNotificationManager.h"
+
+@implementation SBNotificationManager
+
+@end
